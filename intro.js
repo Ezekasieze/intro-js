985bfd00c23ed6efac5fde8fce7186e697b9f513
Ezekasieze <49339450+Ezekasieze@users.noreply.github.com>
2026-03-13 19:16:29 +0100

    Fix console log syntax for favouriteLanguage

--- intro.js
+++ intro.js
@@ -3,4 +3,4 @@ const age = "40";
 const favouriteLanguage = "python";
 console.log("Name:", name);
 console.log("Age:"; age);
-console.log("Favourite Programming Language:"; favouriteLanguage);
+console.log("Favourite Programming Language:", favouriteLanguage);
