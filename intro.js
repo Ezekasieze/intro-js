864f0005f804cf27147a862599bb3fe1167641eb
Ositadinma <ositadinmagreat@gmail.com>
2026-03-13 15:58:04 +0100

    Add intro.js file

---
+++ intro.js
@@ -0,0 +1,6 @@
+const name = "Ositadinma";
+const age = "40;
+const favouriteLanguage = "python";
+console.log("Name:", name);
+console.log("Age:"; age);
+console.log("Favourite Programming Language:"; favouriteLanguage);
